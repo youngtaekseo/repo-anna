0e84eb6a83505e997369d8b254cdc285318e36c2
ramses <ramses69@naver.com>
2024-05-16 17:23:46 +0900

    프로젝트 설정

---
+++ anna/src/main/java/com/stayc/AnnaApplication.java
@@ -0,0 +1,14 @@
+package com.stayc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class AnnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnaApplication.class, args);
+	}
+
+}
---
+++ anna/src/test/java/com/stayc/AnnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
